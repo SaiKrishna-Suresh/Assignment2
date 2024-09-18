a62129bb50b818f0fb3fe9d59311664c6390fa20
Sai Krishna <w0845777@myscc.ca>
2024-09-18 18:28:38 -0400

    increased the array size

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,9 +10,13 @@ import { NgForOf } from '@angular/common';
   templateUrl: './app.component.html',
   styleUrls:[ './app.component.css']
 })
+
 export class AppComponent {
   title = 'Assignment2';
   player1:Players []=[{rank:1,firstName:"Virat",lastName:"kohli",country:"India",average:55},
-  {rank:2,firstName:"Steve",lastName:"Smith",country:"Australia",average:49}];
+  {rank:2,firstName:"Steve",lastName:"Smith",country:"Australia",average:49},
+    {rank:3,firstName:"Rohit",lastName:"Sharma",country:"India",average:45},
+    {rank:4,firstName:"Joe Benjamin",lastName:"Root",country:"England",average:44.3},
+    {rank:5,firstName:"Kane",lastName:"Williamson",country:"New Zealand",average:42.2}];
 
 }
